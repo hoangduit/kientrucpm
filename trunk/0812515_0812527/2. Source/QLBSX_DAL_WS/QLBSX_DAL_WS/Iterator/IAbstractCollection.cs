c9590285e06883606ca2731108628c2b6ad9421f
7hanatos13@gmail.com <7hanatos13@gmail.com@5e1c17dd-d412-8974-da27-29fa92ce3fd3>
2011-12-25 03:01:40 +0000

    Bổ sung Iterator Pattern.

---
+++ trunk/0812515_0812527/2. Source/QLBSX_DAL_WS/QLBSX_DAL_WS/Iterator/IAbstractCollection.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QLBSX_DAL_WS.Iterator
+{
+    public interface IAbstractCollection
+    {
+        Iterator CreateIterator();
+    }
+}
