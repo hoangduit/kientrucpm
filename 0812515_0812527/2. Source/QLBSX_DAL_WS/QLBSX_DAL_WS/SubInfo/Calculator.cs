f86ed00a9a6adbe4c7a48ed8063acb61f3a8bc40
7hanatos13@gmail.com <7hanatos13@gmail.com@5e1c17dd-d412-8974-da27-29fa92ce3fd3>
2011-12-26 02:31:25 +0000

    Chỉnh sửa Adapter Pattern.

---
+++ 0812515_0812527/2. Source/QLBSX_DAL_WS/QLBSX_DAL_WS/SubInfo/Calculator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QLBSX_DAL_WS.SubInfo
+{
+    public class Calculator : ICalculator
+    {
+        public double Eval(double a, double b, double c)
+        {
+            return a * b * c;
+        }
+    }
+}
---
+++ 0812515_0812527/2. Source/QLBSX_DAL_WS/QLBSX_DAL_WS/SubInfo/ICalculator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QLBSX_DAL_WS.SubInfo
+{
+    public interface ICalculator
+    {
+        double Eval(double a, double b, double c);
+    }
+}
