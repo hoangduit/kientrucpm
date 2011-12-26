b89af7f03c713cda10b70d662a4e94a3991064f7
7hanatos13@gmail.com <7hanatos13@gmail.com@5e1c17dd-d412-8974-da27-29fa92ce3fd3>
2011-12-26 02:58:55 +0000

    Bổ sung Proxy Pattern.

---
+++ trunk/0812515_0812527/2. Source/QLBSX_DAL_WS/QLBSX_DAL_WS/SubInfo/CalculatorProxy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QLBSX_DAL_WS.SubInfo
+{
+    public class CalculatorProxy : ICalculator
+    {
+        private Calculator _calculator = new Calculator();
+        public double Eval(double a, double b, double c)
+        {
+            return _calculator.Eval(a, b, c);
+        }
+    }
+}
